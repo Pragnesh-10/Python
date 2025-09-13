b1b3127a4f31c2196548bc1b0f766f9fa8ca924e
Y Naga Pragnesh <yedrunagapragnesh@gmail.com>
2025-09-13 14:52:13 +0530

    Update Password_generator.py

--- Password_generator.py
+++ Password_generator.py
@@ -1,12 +1,12 @@
 import random
 import string
 def password_generator(length=12):
-  letters = string.asiic
+  letters = string.asiic_letters
   number = string.digits
   symbols = string.puncutations
   all_char = letters + number + symbols
-  password = "".join((random.choice(all_char) for _ in range(length))
-  print(password)
-  return password_generator
+  password = "".join([random.choice(all_char) for _ in range(length)])
+  
+  return password_generator()
 print(password_generator(12))                     
   
