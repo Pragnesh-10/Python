42fc4d7d7fdebfc56b150ba72f088c1ac60d0eb5
Y Naga Pragnesh <yedrunagapragnesh@gmail.com>
2025-09-18 12:43:53 +0530

    Update age_calculator.py

--- age_calculator.py
+++ age_calculator.py
@@ -1,1 +1,10 @@
-
+from datetime import date
+def age_calculator(birth_year,birth_month,birth_date):
+  today = date.today()
+  age = today.year-birth_year
+  if (today.month,today.day) < (birth_month,birth_year):
+    age -= 1
+  return age
+birth_year = int(input("enter the year")
+birth_month = int(input("enter month")
+birth_date = int(input("emter date")
